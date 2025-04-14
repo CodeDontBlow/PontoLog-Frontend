4637933df1c6d3c4e328d7a8c0f278c445b5b571
Luana Souza <128157430+luanaapms@users.noreply.github.com>
2025-04-14 18:31:22 -0300

    feat [FE-COMP-6]: Adicionando estrutura component footer

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.grouped}>
+          <div className={styles.column}>
+            <h4 className={styles.title}>Projeto</h4>
+            <ul className={styles.list}>
+              <li><a href="https://www.gov.br/mdic/pt-br/assuntos/comercio-exterior/estatisticas/base-de-dados-bruta">Dados utilizados</a></li>
+              <li><a href="#">Tratamento de Dados</a></li>
+              <li><a href="https://github.com/CodeDontBlow/PontoLog">Github do Projeto</a></li>
+            </ul>
+          </div>
+
+          <div className={styles.column}>
+            <h4 className={styles.title}>Equipe</h4>
+            <ul className={styles.list}>
+              <li><a href="#">Sobre Nós</a></li>
+              <li><a href="https://github.com/CodeDontBlow">Github da Equipe</a></li>
+            </ul>
+          </div>
+        </div>
+        
+        <div className={styles.copyright}>
+          <h4 className={styles.title}>Copyright © CodeDon’tBlow</h4>
+          <p className={styles.list}>Onde cortamos o pavio dos seus problemas!</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
