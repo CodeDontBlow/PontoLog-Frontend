c2da6dce2c70357fd23cabbfe6006a9f3a4d80c4
YgorPereira <ygorrp25@gmail.com>
2025-04-19 15:07:32 -0300

    feature (FE-COMP-5): adicionando função para o botão mudar de cor

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,16 @@
 // importando bibliotecas necessários 
 import { faChartLine, faBalanceScale, faInfo } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 // importando components e assets necessários
 import Button from '../Buttons/Button/Button';
 import logoIcon from '../../assets/logos/LOGOSVG.svg';
 
-
-
 import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
-  
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleClick = (route) => {
     return (
@@ -23,15 +21,18 @@ const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <div className={styles.logo}>
-        <img src={logoIcon} alt="" />
-      </div>  
-
-      <div className={styles.mainButtons}>
-        <Button variant={'Btn-icon'} icon={faChartLine} iconClassname={styles.navButtonIcon} className={styles.navButton} onClick={() => handleClick('/statistics')} />
-        <Button variant={'Btn-icon'} icon={faBalanceScale} iconClassname={styles.navButtonIcon} className={styles.navButton} onClick={() => handleClick('/comparison')} />
+        <Link to={'/'}>
+          <img src={logoIcon} alt="" />
+        </Link>
       </div>
 
-      <Button variant={'Btn-icon'} icon={faInfo} iconClassname={styles.infoIcon} className={styles.infoButton} onClick={() => handleClick('/about')}/>
+      <div className={styles.mainButtons}>
+        <Button variant={'Btn-icon'} icon={faChartLine} iconClassname={styles.navButtonIcon} className={`${styles.navButton} ${location.pathname === '/statistics' ? styles.active : ''}`} onClick={() => handleClick('/statistics')} />
+
+        <Button variant={'Btn-icon'} icon={faBalanceScale} iconClassname={styles.navButtonIcon} className={`${styles.navButton} ${location.pathname === '/comparison' ? styles.active : ''}`} onClick={() => handleClick('/comparison')} active={location.pathname === '/comparison'} />
+      </div>
+
+      <Button variant={'Btn-icon'} icon={faInfo} iconClassname={styles.infoIcon} className={`${styles.infoButton} ${location.pathname === '/about' ? styles.active : ''}`} onClick={() => handleClick('/about')} />
     </div>
   );
 };
