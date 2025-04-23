f4c056d6ed179062a7b575cbd5e0e32738b01215
Manhã <rafagonpin317@gmail.com>
2025-04-23 07:28:27 -0300

    feature[FE-COMP-12] Definindo classes css e mudando px pra rem

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -3,9 +3,9 @@ import styles from "./Input.module.css"
 const Input = ({label , placeholder , type , id}) => {
     return(
         <>
-        <label htmlFor={id}>
-            <p>{label}</p>
-            <input type={type} id={id} placeholder={placeholder}/>
+        <label className={styles.label} htmlFor={id}>
+            <p className={styles.labelTxt}>{label}</p>
+            <input className={styles.input} type={type} id={id} placeholder={placeholder}/>
         </label>
         </>
     )
