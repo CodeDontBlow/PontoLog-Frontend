3ae14c40ceef5dd71a5dd66231fa5cbb3c3fdcb2
YgorPereira <ygorrp25@gmail.com>
2025-06-16 22:01:09 -0300

    fix develop

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://pontolog.hopto.org:3000/",
-    // baseURL: "http://localhost:3000/",
+    baseURL: "http://localhost:3000/",
     headers: {
         "Content-Type": "application/json",
     },
